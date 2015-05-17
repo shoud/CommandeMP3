7ae7503e806d87352ce1f0f38c27ea5f384cfd55
shoud <shoud@hotmail.fr>
2015-05-17 16:18:56 +0200

    Ajout de commentaires

--- src/java/webservice/CommandeMP3WS.java
+++ src/java/webservice/CommandeMP3WS.java
@@ -6,23 +6,30 @@
 package webservice;
 
 import java.util.ArrayList;
-import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 /**
- *
- * @author Thomas
+ * @class CommandeMP3WS le service web permettant de décoder les actions
+ * et les titres dans une chaine de caractères
  */
 @WebService(serviceName = "CommandeMP3WS")
 public class CommandeMP3WS {
 
+    //Les différents mots pour l'action jouer
     private ArrayList<String> listJouer = new ArrayList<String>();
+    //Les différents mot pour l'action supprimer
     private ArrayList<String> listSupprimer = new ArrayList<String>();
+    //Les différents mot pour l'action rechercher
     private ArrayList<String> listRechercher = new ArrayList<String>();
+    //Les différents mot pour l'action ajouter
     private ArrayList<String> listAjouter = new ArrayList<String>();
-    private ArrayList<String> listMusique = new ArrayList<String>();
-     
+    
+    /**
+     * Constructeur du service web
+     * Permet d'initialiser les listes pour reconnaitre les 
+     * différentes actions possiblent 
+     */
     public CommandeMP3WS()
     {
         //Pour jouer
@@ -46,41 +53,56 @@ public class CommandeMP3WS {
         listAjouter.add("rajoute");         
     }
     /**
-     * Web service operation
+     * Méthode getAction permet de définir quel action est voulue par
+     * rapport à une chaine de caractères
+     * @param phrase la phrase reçu à décoder
+     * @return l'action a effectuer sur le client
      */
     @WebMethod(operationName = "getAction")
-    public String getAction(@WebParam(name = "phrase") String phrase) {
-        //TODO write your implementation code here:
+    public String getAction(@WebParam(name = "phrase") String phrase)
+    {
         //Détection de jouer
            for(String jouer : listJouer)
+               //Si un mot dans la phrase est dans la liste jouer
                 if(phrase.toLowerCase().contains(jouer))
+                    //On retourne l'action jouer
                     return "jouer";
 
             //Détection de rechercher
             for(String rechercher : listRechercher)
+                //Si un mot dans la phrase est dans la liste rechercher
                 if(phrase.toLowerCase().contains(rechercher))
+                    //On retourne l'action rechercher
                     return "rechercher";
 
             //Détection de supprimer
             for(String supprimer : listSupprimer)
+                //Si un mot dans la phrase est dans la liste supprimer
                 if(phrase.toLowerCase().contains(supprimer))
-                   return "supprimer";
+                    //On retourne l'action supprimer
+                    return "supprimer";
 
             //Détection d'ajouter
             for(String ajouter : listAjouter)
+                //Si un mot dans la phrase est dans la liste ajouter
                 if(phrase.toLowerCase().contains(ajouter))
+                    //On retourne l'action ajouter
                     return "ajouter";
 
-            //return commande.erreur;
+            //Si aucune action n'est trouvé, on retourne l'action erreur
             return "erreur";
     }
 
     /**
-     * Web service operation
+     * Méthode permettant de retourner le titre présent dans une chaine de caractères
+     * @param phrase La chaine de caractères à décoder
+     * @return titre le titre de la chanson
      */
     @WebMethod(operationName = "getTitre")
-    public String getTitre(@WebParam(name = "phrase") String phrase) {
-        //TODO write your implementation code here:
-        return phrase;
+    public String getTitre(@WebParam(name = "phrase") String phrase)
+    {
+        String titre = phrase;
+        //On retourne le titre trouvé
+        return titre;
     }
 }
